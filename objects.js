1c8f7efdc9f9b0a6ea8767586e1b68451d0e66ad
William Ulman <will.ulman@gmail.com>
2019-01-04 00:24:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,9 @@
 var playlist = {
   'SOPHIE': 'Immaterial',
   'Yuck': 'The Wall',
-  
+  'Carly Rae Jepsen': 'I Didn\'t Just Come Here to Dance'
+}
+
+function updatePlaylist (playlist, artist, song) {
+  playlist.artist = song;
+}
